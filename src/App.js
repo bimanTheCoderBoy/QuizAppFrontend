54c272b7747a2f67e4cb8f03cf35d415a2604a19
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2023-07-23 14:53:31 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-
-    </div>
-  );
-}
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavbarComponent from '../components/NavbarComponent';
+
+function HomePage() {
+    return (
+        <>
+            <NavbarComponent />
+            <h1>Aluu</h1>
+        </>
+    )
+}
+
+export default HomePage
