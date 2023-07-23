fb37ff5e9529ed4ce274c6ff46d7ae015985d5d6
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2023-07-23 14:56:54 +0530

    check

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -12,7 +12,7 @@ const NavbarComponent = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <NavLink to="/">Help</NavLink>
+                        <NavLink to="/help">Help</NavLink>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
