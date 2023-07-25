0f54db7cc9df2f931cf1b617f75ab9656bb8ef06
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2023-07-25 22:18:02 +0530

    Login Design

--- src/context/ProfileContext.js
+++ src/context/ProfileContext.js
@@ -1,26 +1,26 @@
-import { createContext, useContext, useReducer } from "react";
-import ProfileReducer from "../reducer/ProfileReducer";
-import axios from 'axios';
+// import { createContext, useContext, useReducer } from "react";
+// import ProfileReducer from "../reducer/ProfileReducer";
+// import axios from 'axios';
 
-const ProfileContext = createContext();
+// const ProfileContext = createContext();
 
-const initialStage = {
-    isLoading: false,
-    isError: false,
-    isLogin: false,
-    profile: {},
-    userId: ""
-}
+// const initialStage = {
+//     isLoading: false,
+//     isError: false,
+//     isLogin: false,
+//     profile: {},
+//     userId: ""
+// }
 
-const ProfileProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(ProfileReducer, initialStage);
+// const ProfileProvider = ({ children }) => {
+//     const [state, dispatch] = useReducer(ProfileReducer, initialStage);
 
-    const getProfile = async (url) => {
-        dispatch({ type: "SET_LOADING" });
-        try {
-            const
-        } catch (error) {
-            dispatch({ type: "API_ERROR" })
-        }
-    }
-}
+//     const getProfile = async (url) => {
+//         dispatch({ type: "SET_LOADING" });
+//         try {
+//             const
+//         } catch (error) {
+//             dispatch({ type: "API_ERROR" })
+//         }
+//     }
+// }
