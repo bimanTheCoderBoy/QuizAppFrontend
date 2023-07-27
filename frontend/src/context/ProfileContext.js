7154ac6a953873eb502b30355a43487e6c9cb8cc
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2023-07-27 22:27:03 +0530

    ProfileContext

--- frontend/src/context/ProfileContext.js
+++ frontend/src/context/ProfileContext.js
@@ -1,26 +1,81 @@
-// import { createContext, useContext, useReducer } from "react";
-// import ProfileReducer from "../reducer/ProfileReducer";
-// import axios from 'axios';
+import { createContext, isValidElement, useContext, useReducer } from "react";
+import ProfileReducer from "../reducer/ProfileReducer";
+import axios, { isAxiosError } from 'axios';
 
-// const ProfileContext = createContext();
+const ProfileContext = createContext();
 
-// const initialStage = {
-//     isLoading: false,
-//     isError: false,
-//     isLogin: false,
-//     profile: {},
-//     userId: ""
-// }
+const initialState = {
+    isLoading: false,
+    isError: false,
+    errorMsg: "",
+    isLogin: false,
+    profile: {},
+    userId: ""
+}
 
-// const ProfileProvider = ({ children }) => {
-//     const [state, dispatch] = useReducer(ProfileReducer, initialStage);
+const ProfileProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(ProfileReducer, initialState);
 
-//     const getProfile = async (url) => {
-//         dispatch({ type: "SET_LOADING" });
-//         try {
-//             const
-//         } catch (error) {
-//             dispatch({ type: "API_ERROR" })
-//         }
-//     }
-// }
+    // GET PROFILE 
+    const getProfile = async (url) => {
+        dispatch({ type: "SET_LOADING" });
+        try {
+            const resp = await axios.get(url);
+            const profile = resp.data;
+            console.log(profile);
+            dispatch({ type: "MY_PROFILE", payload: profile });
+        } catch (error) {
+            dispatch({ type: "API_ERROR", payload: error });
+        }
+    }
+
+
+    // LOGIN API 
+    const userLogin = async (url, body) => {
+        try {
+            const resp = await axios.post(url,
+                JSON.stringify({}),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(resp);
+            dispatch({ type: "LOGIN_SUCCESS", payload: resp });
+        } catch (error) {
+            dispatch({ type: "LOGIN_ERROR", payload: error })
+        }
+    }
+
+    // REGISTRATION API 
+    const userRegistration = async (url, body) => {
+        try {
+            const resp = await axios.post(url,
+                JSON.stringify({}),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(resp);
+            dispatch({ type: "LOGIN_SUCCESS", payload: resp });
+        } catch (error) {
+            dispatch({ type: "LOGIN_ERROR", payload: error })
+        }
+    }
+
+
+    return <ProfileContext.Provider value={{ ...state, getProfile, userLogin }}>
+        {children}
+    </ProfileContext.Provider>
+
+}
+
+
+// custom hooks
+const useProfileContext = () => {
+    return useContext(ProfileContext);
+}
+
+
+export { ProfileProvider, ProfileContext, useProfileContext };
--- frontend/src/reducer/ProfileReducer.js
+++ frontend/src/reducer/ProfileReducer.js
@@ -1,5 +1,50 @@
 const ProfileReducer = (state, action) => {
 
+    // GET PROFILE 
+    if (action.type === "SET_LOADING") {
+        return {
+            ...state,
+            isLoading: true
+        }
+    }
+    else if (action.type === "MY_PROFILE") {
+        return {
+            ...state,
+            isLoading: false,
+            profile: action.payload
+        }
+    }
+    else if (action.type === "API_ERROR") {
+        return {
+            ...state,
+            isLoading: false,
+            isError: true,
+            errorMsg: action.payload,
+        }
+    }
+
+
+    //LOGIN
+    else if (action.type === "LOGIN_SUCCESS") {
+        return {
+            ...state,
+            isLogin: true,
+            userId: action.payload,
+        }
+    }
+    else if (action.type === "LOGIN_ERROR") {
+        return {
+            ...state,
+            isError: true,
+            errorMsg: action.payload,
+        }
+    }
+
+    else {
+        return {
+            ...state,
+        }
+    }
 }
 
 export default ProfileReducer;
