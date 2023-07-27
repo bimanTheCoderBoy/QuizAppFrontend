650dfce0e5ccffcb4cf480f49418690f958df96e
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-07-28 01:23:06 +0530

    jk

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,6 +1,6 @@
 const express =require('express')
 const router=express.Router()
-const {register,login,isAuth}=require("../controller/loginController")
+const {register,login,isAuth}=require("../controller/AuthController")
 
 
 router.get("",(req, res)=>{
