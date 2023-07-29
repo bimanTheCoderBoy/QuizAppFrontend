ca906ecdda9a8049106a4c27e215b84f8a3ec47f
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-07-30 00:42:59 +0530

    jkl;

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -1,110 +1,109 @@
 
-const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-const User = require("../models/user");
-const sendToken = require("../utils/sendToken");
+const bcrypt=require("bcrypt");
+const jwt=require('jsonwebtoken');
+const User =require("../models/user");
+const sendToken=require("../utils/sendToken");
 const { ErrorHandler } = require('../utils/error')
 
 
 
-const register = async (req, res, next) => {
+const register=async (req,res,next)=>{
 
-  try {
-
-
-    const { name, email, password, type } = req.body;
-    let user = null;
-
-    // user = await User.findOne({ email: email });
-
-
-    // if (user) {
-    //   next(new ErrorHandler("user aldready exits", 404));
-    // }
-    // console.log(req.body);
-
-
-    // const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        
+    
+    const {name, email, password,role}=req.body;
+    let user =null
+    
+        user = await User.findOne({email:email})
+   
+   
+    if(user) 
+    {
+            next(new ErrorHandler("user aldready exits",404))
+    }
 
 
-console.log("check 1");
-    const newUser = await User.create({ name, email, password, type });
-console.log("check 2");
-    sendToken(res, newUser, "seccessfully registered");
+    const hashedPassword=await bcrypt.hash(password,10)
+    
+    //skey setting
+    const skey=Math.floor(Math.random()*9000000000) + 1000000000;
+    // hashedSkey= await bcrypt.hash(skey+"",10)
 
-  }
+
+  user=await  User.create({name,email,password:hashedPassword,role,skey})
+
+   sendToken(res,user,"seccessfully registered");
+  // res.send("asd")
+  
+}
   catch (error) {
-    // next(new ErrorHandler("database error", 404))
-    res.send(error)
+    next(new ErrorHandler(error.message||"database error",404))
   }
-
+  
 
 }
 
 
 
-const login = async (req, res, next) => {
 
+const login= async(req,res,next)=>{
   try {
-
-
-    const { email, password } = req.body;
-    const user = await User.findOne({ email }).select("+password")
-    if (!user) {
-      next(new ErrorHandler("user does not exits", 404))
-    }
-    const isMatched = await bcrypt.compare(password, user.password)
-    if (!isMatched) {
-      next(new ErrorHandler("wrong password", 404))
-    }
-
-
-    sendToken(res, user, "login successfull")
-  } catch (error) {
-
-    console.log('Error in Login', error)
-    next(new ErrorHandler("database error", 404))
+      
+ 
+  const {email,password}=req.body;
+  const user=await User.findOne({email}).select("+password").select("+skey")
+  if(!user){
+       next(new ErrorHandler("user does not exits",404))
   }
+ var isMatched=await bcrypt.compare(password,user.password)
+ console.log(bcrypt.getRounds(user.password))
+ 
+ if(!isMatched){
+         next(new ErrorHandler("wrong password",404))
+ }
 
+if(user && isMatched){
+sendToken(res,user,"login successfull")
+}
+
+} catch (error) {
+  next(new ErrorHandler("database error",404))
+}
 
 }
 
 
+const isAuth= async(req,res,next)=>{
+  const {token} =req.cookies
+  if(token){
+      const decodedId= jwt.verify(token,process.env.JWT_SECRET)
 
-
-
-const isAuth = async (req, res, next) => {
-  const { token } = req.cookies
-  if (token) {
-    const decodedId = jwt.verify(token, process.env.JWT_SECRET)
-
-    if (decodedId) {
+     if(decodedId){
       try {
-        req.user = await User.findById(decodedId);
-        next()
+          req.user=await User.findById(decodedId);
+          next()
         //   res.json({
         //     success:true,
         //     message:"ok"
         // })
       } catch (error) {
 
-        console.log(error)
-        res.json({
-          success: false,
-          message: "internal server isuee"
+          console.log(error)
+          res.json({
+            success:false,
+            message:"internal server isuee"
         })
       }
-
-
-    }
+      
+      
+     }
   }
-  else {
-    res.json({
-      success: false,
-      message: "user is not Authenticated"
-    })
-  }
+else {
+  res.json({
+      success:false,
+      message:"user is not Authenticated"
+  })}
 }
 
-module.exports = { register, login, isAuth };
-
+module.exports={register,login,isAuth};
